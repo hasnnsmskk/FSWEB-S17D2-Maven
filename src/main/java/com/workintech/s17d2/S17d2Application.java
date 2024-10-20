e7acab8586c8435e85f75548d50f17740e58b073
Hasan Şimşek <168709291+hasnnsmskk@users.noreply.github.com>
2024-10-21 01:55:51 +0300

    Project completed.

--- src/main/java/com/workintech/s17d2/S17d2Application.java
+++ src/main/java/com/workintech/s17d2/S17d2Application.java
@@ -1,7 +1,9 @@
 package com.workintech.s17d2;
 
+import com.workintech.s17d2.tax.DeveloperTax;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class S17d2Application {
@@ -10,4 +12,8 @@ public class S17d2Application {
 		SpringApplication.run(S17d2Application.class, args);
 	}
 
+	@Bean
+	public DeveloperTax developerTax() {
+		return new DeveloperTax();
+	}
 }
